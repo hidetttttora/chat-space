7f08489f9bfbfac879294a3a555a002bfc19cce2
hidetttttora <50098627+hidetttttora@users.noreply.github.com>
2019-06-17 11:33:10 +0900

    Update staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -49,9 +49,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+# server '52.196.231.217',
+#   user: 'ec2-user',
+#   roles: %w{app db web},
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
